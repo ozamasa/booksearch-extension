58dc8b23448b0a725a103ce94bbf91e961a402d7
Masaki Ozawa <ozamasa123@gmail.com>
2017-01-08 18:33:16 +0900

    change css & view

--- popup.js
+++ popup.js
@@ -1,7 +1,11 @@
 chrome.tabs.executeScript( {
   code: "window.getSelection().toString();"
 }, function(selection) {
+  if (selection == "undefined"){
+    selection = "";
+  }
+  var select = "<h4>選択文字列：" + selection + "</h3>"
   var amazon = "<li><a href='https://www.amazon.co.jp/s?field-keywords=" + selection + "'>アマゾン</a></li>";
   var calil  = "<li><a href='https://calil.jp/search?q=" + selection + "'>カーリル</a></li>";
-  document.getElementById("output").innerHTML = selection + "<ul>" + amazon + calil + "</ul>";
+  document.getElementById("output").innerHTML = select + "<ul>" + amazon + calil + "</ul>";
 });
