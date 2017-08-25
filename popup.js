efb9155876e93b5c0c4b11064bcfc67a3902a5b8
Masaki Ozawa <ozamasa123@gmail.com>
2017-08-25 15:45:27 +0900

    change amazon link

--- popup.js
+++ popup.js
@@ -5,7 +5,7 @@ chrome.tabs.executeScript( {
     selection = "";
   }
   var select = "<h4>選択文字列：" + selection + "</h3>"
-  var amazon = "<li><a href='https://www.amazon.co.jp/s?field-keywords=" + selection + "'>アマゾン</a></li>";
+  var amazon = "<li><a href='https://www.amazon.co.jp/s/ref=assoc_res_sw_zz_dkp_cra_t0_view_all?search-alias=aps&tag=ozamart-22&linkCode=w13&linkId=&field-keywords=" + selection + "'>アマゾン</a></li>";
   var calil  = "<li><a href='https://calil.jp/search?q=" + selection + "'>カーリル</a></li>";
   document.getElementById("output").innerHTML = select + "<ul>" + amazon + calil + "</ul>";
 });
